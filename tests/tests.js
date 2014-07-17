3cc07379b4202cd2b49bfb0b01b1f6b37741f6e2
Konstantin Käfer <mail@kkaefer.com>
2014-07-17 12:21:57 -0700

    add keys, publish script, tests [publish binary]

--- tests/tests.js
+++ tests/tests.js
@@ -12,10 +12,10 @@ assert.equal(glslOptimizer.VERTEX_SHADER, 0, 'exports VERTEX_SHADER');
 assert.equal(glslOptimizer.FRAGMENT_SHADER, 1, 'exports FRAGMENT_SHADER');
 assert.equal(typeof glslOptimizer.Compiler, 'function', 'exports Compiler constructor');
 
-var ESSL = false; 
+var ESSL = false;
 
 //First we need to create a new compiler
-var compiler = new glslOptimizer.Compiler(2);
+var compiler = new glslOptimizer.Compiler(glslOptimizer.TARGET_OPENGL);
 
 assert.equal(typeof compiler.dispose, 'function', 'exports Compiler#dispose()');
 
@@ -26,12 +26,11 @@ var raw = shader.rawOutput();
 var output = shader.output();
 var log = shader.log();
 
+assert.ok(didCompile);
 assert.equal(typeof didCompile, 'boolean', 'exports compiled()');
 assert.equal(typeof raw, 'string', 'exports rawOutput()');
 assert.equal(typeof output, 'string', 'exports output()');
 assert.equal(typeof log, 'string', 'exports log()');
-
-console.log(output, didCompile);
 
 //Clean up shader...
 shader.dispose();

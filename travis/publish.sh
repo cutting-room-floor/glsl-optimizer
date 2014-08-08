902a049060b0694992ace18de0950c108ce4c241
Dane Springmeyer <dane@mapbox.com>
2014-08-08 13:39:44 -0700

    build fixes [publish binary]

--- travis/publish.sh
+++ travis/publish.sh
@@ -14,14 +14,12 @@ COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 if test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"
     then
 
-    npm install aws-sdk
-    ./node_modules/.bin/node-pre-gyp package testpackage
-    ./node_modules/.bin/node-pre-gyp publish info
-
+    node-pre-gyp package
+    node-pre-gyp testpackage
+    node-pre-gyp publish
+    node-pre-gyp info
     rm -rf build
-    rm -rf lib/blend.node
+    node-pre-gyp clean
     npm install --fallback-to-build=false
     npm test
-
-    node-pre-gyp info
 fi
